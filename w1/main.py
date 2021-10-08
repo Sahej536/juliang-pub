0e47c3b6283b1b8f518aea0250ea95bac21ef238
snowlizard <illegal_burrito@live.com>
2021-10-08 14:58:37 -0700

    w1

---
+++ w1/main.py
@@ -0,0 +1,24 @@
+#Data types 
+
+#String 
+print("Hello"[0])
+
+#Integer
+
+print(123 + 234)
+
+#Float
+
+print(3.143542392)
+
+
+#Boolean
+
+running = False
+
+
+# I was looking at data types and thought something was missing
+# so I looked around for null and couldn't find it but found that 
+# python handles null data by using the None keyword.
+# I thinking knowing None is important for comparing values later on.
+None
---
+++ w1/readme.md
@@ -0,0 +1,4 @@
+## Program thoughts
+A program I would be interested in making would be a bot that scrapes the
+web. I don't know what kind of information it would get right now but I think
+it would be a good idea.
